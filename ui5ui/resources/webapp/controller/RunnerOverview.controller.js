609ab748d3a5d25bc99b7c13d1e74b29da2aa16a
Paul Bläsi <paul.blaesi@exxcellent.de>
2018-04-30 15:21:47 +0200

    first running version of running :-)

--- ui5ui/resources/webapp/controller/RunnerOverview.controller.js
+++ ui5ui/resources/webapp/controller/RunnerOverview.controller.js
@@ -4,6 +4,14 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("ui5ui.controller.RunnerOverview", {
-
+		formatGenderIconURI: function(genderValue) {
+			if (genderValue === 'M') {
+				return 'sap-icon://customer';
+			} else if (genderValue === 'W') {
+				return 'sap-icon://physical-activity';
+			} else {
+				return 'sap-icon://sys-help';
+			}
+		}
 	});
 });
