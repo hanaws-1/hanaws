2e2b1c52552fb2960fd32cc18d03717cde22acca
pblaesi <paul.blaesi@exxcellent.de>
2018-05-01 23:04:54 +0200

    revert attempts to create contexts

--- ui5ui/resources/webapp/service/AppointmentService.js
+++ ui5ui/resources/webapp/service/AppointmentService.js
@@ -78,18 +78,29 @@ sap.ui.define([
 				return _update(oDataModel, appointmentId, data);
 			}
 		},
-		createNewAppointment: function(runnerData) {
-			var runnerName = runnerData ? runnerData.name : "";
-			var runnerId = runnerData ? runnerData.id : null;
-			return Promise.resolve({
-				"Runner": {
-					name: runnerName
-				},
-				"runnerId": runnerId,
-				"time": new Date(),
-				comment: 'New comment',
-				success: 0
-			});
+		createNewAppointment: function(oDataModel, runnerPath) {
+      var runnerContextBinding = oDataModel.bindContext(runnerPath);
+			return new Promise(function (resolve, reject) {
+        runnerContextBinding.attachChange(function (changeEvent) {
+        	resolve(runnerContextBinding);
+        });
+        runnerContextBinding.initialize();
+      }).then(function (initializedRunnerBinding) {
+      	return initializedRunnerBinding.getContext().requestProperty('/id');
+      }).then(function (runnerId) {
+        var appointmentsPath = runnerPath + '/' + 'appointments';
+        var listBinding = oDataModel.bindList(appointmentsPath);
+        runnerContextBinding.destroy();
+        return listBinding.create({
+          "Runner": {
+            name: runnerName
+          },
+          "runnerId": runnerId,
+          "time": new Date(),
+          comment: 'New comment',
+          success: 0
+        });
+      });
 		}
 	};
 });
